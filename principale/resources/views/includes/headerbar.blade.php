5b998cdbb82345aa529ccf9aa0bc465187cb5f42
Duc-Steve <stevelioni600@gmail.com>
2024-11-28 16:56:27 +0000

    correction de style

---
+++ principale/resources/views/includes/headerbar.blade.php
@@ -0,0 +1,95 @@
+
+            <nav class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-2 py-lg-0">
+                <!-- Bouton de Menu -->
+                <div class="menu-toggle align-items-center d-flex gap-3 me-4">
+                    <button id="menuToggle" class="bg-sociprodd-jaune text-sociprodd-bleuefoncee br-sociprodd-1 bdr-sociprodd-all-bleuefoncee">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                            <path d="M3 4H21V6H3V4ZM3 11H21V13H3V11ZM3 18H21V20H3V18Z"></path>
+                        </svg>
+                    </button>
+
+                    <!-- Pour Téléphone -->
+                    <span class="text-sociprodd-bleuefoncee d-block d-lg-none">Menu</span>
+                    
+                    <!-- Pour Ordinateur -->
+                    <span class="text-menu d-none d-lg-block">Menu</span>
+                </div>
+
+
+                <a href="https://sociprodd.org" class="bestP d-flex">
+                    <img src="/principale/public/assets/images/Logo-SOCIPRODD.png" alt="Logo" width="79px" height="79px">
+                </a>
+
+                <!-- Superposition sombre -->
+                <div id="overlaywindow" class="overlaywindow"></div>
+
+                <!-- Menu -->
+                <div id="menu" class="menu-container">
+                    <div class="menu-header">
+                        <img src="/principale/public/assets/images/Logo-SOCIPRODD.png" alt="Logo SOCIPRODD" class="menu-logo">
+                        <button id="menuClose" class="btn-close-window">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
+                                <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 10.5858L9.17157 7.75736L7.75736 9.17157L10.5858 12L7.75736 14.8284L9.17157 16.2426L12 13.4142L14.8284 16.2426L16.2426 14.8284L13.4142 12L16.2426 9.17157L14.8284 7.75736L12 10.5858Z"></path>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div class="menu-body">
+                        <strong>Nos réalisations</strong>
+                        <strong>Nos actualités</strong>
+                        <strong>Nos podcasts</strong>
+                        <strong>Nos missions</strong>
+                        <div class="menu-section">
+                            <strong>Ce que nous faisons</strong>
+                            <ul>
+                                <li>. Projet A</li>
+                                <li>. Projet B</li>
+                                <li>. Projet C</li>
+                            </ul>
+                        </div>
+                        <div class="menu-section">
+                            <strong>Découvrir SOCIPRODD</strong>
+                            <ul>
+                                <li>. L'organisation</li>
+                                <li>. Localisation</li>
+                                <li>. Contactez-nous</li>
+                                <li>. Bilan annuel 2023</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+
+                    <!-- Pour Ordinateur -->
+                    <div class="navbar-nav ms-auto py-0">
+                            
+                        <a href="" class="d-none d-lg-block nav-item nav-link">Ce que nous faisons</a>
+                        <a href="" class="d-none d-lg-block nav-item nav-link">Découvrir SOCIPRODD</a>
+                        <a href="" class="d-none d-lg-block nav-item nav-link">Agir avec nous</a>
+                        
+                        <!-- Pour Ordinateur -->
+                        <a href="https://pays.sociprodd.org" target="_blank" class="d-none d-lg-block nav-item nav-link" style="margin-left: 5em;">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(218,202,12,1)"><path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM16.0043 12.8777C15.6589 12.3533 15.4097 11.9746 14.4622 12.1248C12.6717 12.409 12.4732 12.7224 12.3877 13.2375L12.3636 13.3943L12.3393 13.5597C12.2416 14.2428 12.2453 14.5012 12.5589 14.8308C13.8241 16.1582 14.582 17.115 14.8116 17.6746C14.9237 17.9484 15.2119 18.7751 15.0136 19.5927C16.2372 19.1066 17.3156 18.3332 18.1653 17.3559C18.2755 16.9821 18.3551 16.5166 18.3551 15.9518V15.8472C18.3551 14.9247 18.3551 14.504 17.7031 14.1314C17.428 13.9751 17.2227 13.881 17.0582 13.8064C16.691 13.6394 16.4479 13.5297 16.1198 13.0499C16.0807 12.9928 16.0425 12.9358 16.0043 12.8777ZM12 3.83333C9.68259 3.83333 7.59062 4.79858 6.1042 6.34896C6.28116 6.47186 6.43537 6.64453 6.54129 6.88256C6.74529 7.34029 6.74529 7.8112 6.74529 8.22764C6.74488 8.55621 6.74442 8.8672 6.84992 9.09302C6.99443 9.40134 7.6164 9.53227 8.16548 9.64736C8.36166 9.68867 8.56395 9.73083 8.74797 9.78176C9.25405 9.92233 9.64554 10.3765 9.95938 10.7412C10.0896 10.8931 10.2819 11.1163 10.3783 11.1717C10.4286 11.1356 10.59 10.9608 10.6699 10.6735C10.7307 10.4547 10.7134 10.2597 10.6239 10.1543C10.0648 9.49445 10.0952 8.2232 10.268 7.75495C10.5402 7.01606 11.3905 7.07058 12.012 7.11097C12.2438 7.12589 12.4626 7.14023 12.6257 7.11976C13.2482 7.04166 13.4396 6.09538 13.575 5.91C13.8671 5.50981 14.7607 4.9071 15.3158 4.53454C14.3025 4.08382 13.1805 3.83333 12 3.83333Z"></path></svg>
+                            Site mondial
+                        </a>
+                    </div>
+
+                    <a href="#" class="d-none d-lg-block br-sociprodd-1 bg-sociprodd-jaune text-sociprodd-bleuefoncee py-2 px-4 my-3 my-lg-0 flex-shrink-0 d-none d-lg-block">Donner</a>
+                 
+                </div>
+            </nav>
+
+            <!-- Pour Téléphone -->
+            <div class="d-block d-lg-none">
+                <div class="d-flex justify-content-between align-items-center bg-sociprodd-bleueclaire py-2 px-3">
+
+                    <a href="https://pays.sociprodd.org" target="_blank" class="text-sociprodd-blanc">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(218,202,12,1)"><path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM16.0043 12.8777C15.6589 12.3533 15.4097 11.9746 14.4622 12.1248C12.6717 12.409 12.4732 12.7224 12.3877 13.2375L12.3636 13.3943L12.3393 13.5597C12.2416 14.2428 12.2453 14.5012 12.5589 14.8308C13.8241 16.1582 14.582 17.115 14.8116 17.6746C14.9237 17.9484 15.2119 18.7751 15.0136 19.5927C16.2372 19.1066 17.3156 18.3332 18.1653 17.3559C18.2755 16.9821 18.3551 16.5166 18.3551 15.9518V15.8472C18.3551 14.9247 18.3551 14.504 17.7031 14.1314C17.428 13.9751 17.2227 13.881 17.0582 13.8064C16.691 13.6394 16.4479 13.5297 16.1198 13.0499C16.0807 12.9928 16.0425 12.9358 16.0043 12.8777ZM12 3.83333C9.68259 3.83333 7.59062 4.79858 6.1042 6.34896C6.28116 6.47186 6.43537 6.64453 6.54129 6.88256C6.74529 7.34029 6.74529 7.8112 6.74529 8.22764C6.74488 8.55621 6.74442 8.8672 6.84992 9.09302C6.99443 9.40134 7.6164 9.53227 8.16548 9.64736C8.36166 9.68867 8.56395 9.73083 8.74797 9.78176C9.25405 9.92233 9.64554 10.3765 9.95938 10.7412C10.0896 10.8931 10.2819 11.1163 10.3783 11.1717C10.4286 11.1356 10.59 10.9608 10.6699 10.6735C10.7307 10.4547 10.7134 10.2597 10.6239 10.1543C10.0648 9.49445 10.0952 8.2232 10.268 7.75495C10.5402 7.01606 11.3905 7.07058 12.012 7.11097C12.2438 7.12589 12.4626 7.14023 12.6257 7.11976C13.2482 7.04166 13.4396 6.09538 13.575 5.91C13.8671 5.50981 14.7607 4.9071 15.3158 4.53454C14.3025 4.08382 13.1805 3.83333 12 3.83333Z"></path></svg>
+                        Site mondial
+                    </a>
+
+                    <a href="#" class="br-sociprodd-1 bg-sociprodd-jaune text-sociprodd-bleuefoncee p-2 px-3">Donner</a>
+                </div>
+            </div>
